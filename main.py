bd9ee793685ff05f8d7d39c3e3d9b8bb2cd25f69
JulianoJk <tzouliano10@gmail.com>
2021-07-29 15:22:59 +0300

    changed the JSON and major problems

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-import question_1 from /home/noxseras/Desktop/duolingo_clone/questions.json
+
 
 
 # Save the json file to a variable
@@ -16,15 +16,16 @@ question = data["question_1"][0]["object"]
 
 #Create and initialize a counter to display in front of each answer the number( e.g. 1. {answer1}, 2. {answer2})
 count = 1
+
 #Append each element from the json object to the list
 for i in data["question_1"][0]["answer"]["wrong_answers"]:
     #Append the number with the wrong answer to the list
-    allAnswers.append(str(count) +'. ' + i)
+    allAnswers.append(i)
     #Increase the counter
     count+=1
 
 #Append the correct answer to the allAnswers list 
-allAnswers.append(str(count) +'. ' + right)
+allAnswers.append(right)
 
 out = '\n'.join(allAnswers)
 
@@ -32,18 +33,29 @@ out = '\n'.join(allAnswers)
 print(question)
 print(out)
 
-def answerInput(userAnswer):
-    while(True):
-        try:
-            userAnswer = int(input("Type the number of your answer: "))
-            #if the answer is not greater or less than the count, stop the loop
-            if userAnswer <= count and userAnswer>0:
-                break
-            else:
-                print("\nPlease type only the number from the displayed numbers!")
-                continue
-        except ValueError:
-            print("\nPlease type only the number from the displayed numbers, not anything else!")
-            continue
+userAnswer = int()
 
-answerInput(None)
+while(True):
+    try:
+        # Take user's input and substract one in order to compare the answer later( the user will not enter 0, 1, but 1, 2..)
+        userAnswer = int(input("Type the number of your answer: ")) -1
+        #if the answer is not greater or less than the count, stop the loop
+        if userAnswer < len(allAnswers) and userAnswer>=0:
+            break
+        else:
+            #Inform the user 
+            print("\nPlease type only the number from the displayed numbers!")
+            continue
+    except ValueError:
+        print("\nPlease type only the number from the displayed numbers, not anything else!")
+        continue
+
+
+try:
+    # if the index of the list, of the user's input, is the right answer, display the message
+    if allAnswers[userAnswer] == right:
+        print("It is right, you picked: " + allAnswers[userAnswer])
+    else:
+        print("Not Worked cuz you picked " + allAnswers[userAnswer])
+except IndexError:
+    print("Wrong")
