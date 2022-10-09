bc74a5abf5cd7574a1c8ba82a1234f4064ded257
Juliano <tzouliano10@gmail.com>
2022-10-09 15:32:38 +0300

    wip

--- front-end/src/components/Navigation/Navigation.tsx
+++ front-end/src/components/Navigation/Navigation.tsx
@@ -1,51 +1,126 @@
-import { Link } from 'react-router-dom';
-import Menu from '../../components/Header/Menu/Menu';
-import { useNavigate } from 'react-router-dom';
-import { useTaskDispatch, useTaskState } from '../../context/UserContext';
-import { Button } from '../button/Button.component';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useUserDispatch, useUserState } from "../../context/UserContext";
+// import styles from "./Navigation.module.css";
+import { useEffect } from "react";
+import { Logout, Home, User, Login, Pencil } from "tabler-icons-react";
+import { Button, Group, Header } from "@mantine/core";
 
 const Navigation: React.FC = () => {
-	const taskDispatch = useTaskDispatch();
-	const taskState = useTaskState();
-	const isLoggedIn = taskState.isLoggedIn;
-	const navigate = useNavigate();
+  const navigate = useNavigate();
+  let userIsLoggedInLocal = localStorage.getItem("user");
 
-	const logOut = () => {
-		taskDispatch({ type: 'RESET_STATE' });
-		navigate('/');
-	};
+  // Dispatch for the user
+  const userDispatch = useUserDispatch();
 
-	if (isLoggedIn) {
-		return (
-			<header>
-				<nav>
-					<div className="d-flex flex-row-reverse bd-highlight">
-						<div className="btn-group flex-wrap" role="group">
-							<Link to="/home" className="btn btn-outline-primary text-light text-opacity-75">
-								Home
-							</Link>
-							<Link to="/profile" className="btn btn-outline-primary text-white text-opacity-75">
-								Profile
-							</Link>
-						</div>
-					<div className="d-grid gap-2">
-						<Button text={'logOut'} onClick={logOut}/>
-					</div>
-					</div>
-				</nav>
-			</header>
-		);
-	} else {
-		return (
-			<header>
-				<nav>
-					<div className="d-flex flex-row-reverse bd-highlight">
-						<Menu />
-					</div>
-				</nav>
-			</header>
-		);
-	}
+  const { user } = useUserState();
+
+  useEffect(() => {
+    // After refresh, restore user's data to be logged
+    if (user.token === undefined && userIsLoggedInLocal !== null) {
+      userDispatch({ type: "SET_USER", user: JSON.parse(userIsLoggedInLocal) });
+    }
+  }, [user]);
+
+  // After logout, clear the context for the user and tasks, then navigate to index
+  const logOut = () => {
+    userDispatch({ type: "RESET_STATE" });
+    userDispatch({ type: "RESET_STATE" });
+    navigate("/");
+  };
+
+  // Check if is user is logged or not
+  if (userIsLoggedInLocal) {
+    return (
+      <Header
+        height={70}
+        p="md"
+        // classNames={{
+        //   root: `${styles.bg}`,
+        // }}
+      >
+        <Group position="right">
+          <Button
+            component={Link}
+            to="/home"
+            radius="md"
+            size="md"
+            leftIcon={<Home size={16} />}
+            uppercase
+          >
+            Home
+          </Button>
+
+          <Button
+            leftIcon={<User size={16} />}
+            radius="md"
+            size="md"
+            uppercase
+            color="yellow"
+            m={1}
+            component={Link}
+            to="/profile"
+          >
+            Profile
+          </Button>
+          <Button
+            leftIcon={<Logout size={16} />}
+            radius="md"
+            color="red"
+            size="md"
+            uppercase
+            component={Link}
+            to="/"
+            m={1}
+            onClick={logOut}
+          >
+            logOut
+          </Button>
+        </Group>
+      </Header>
+    );
+  } else {
+    return (
+      <Header
+        height={70}
+        p="md"
+        // classNames={{
+        //   root: `${styles.bg}`,
+        // }}
+      >
+        <Group position="right">
+          <Button component={Link} to="/" radius="md" size="md" uppercase>
+            Index
+          </Button>
+
+          <Button
+            leftIcon={<Login size={16} />}
+            radius="md"
+            size="md"
+            uppercase
+            color="green"
+            variant="outline"
+            m={1}
+            component={Link}
+            to="/login"
+          >
+            Log-In
+          </Button>
+          <Button
+            leftIcon={<Pencil size={16} />}
+            radius="md"
+            size="md"
+            uppercase
+            component={Link}
+            to="/register"
+            color="green"
+            m={1}
+          >
+            Register
+          </Button>
+        </Group>
+      </Header>
+    );
+  }
 };
-
 export default Navigation;
