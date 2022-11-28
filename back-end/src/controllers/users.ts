cbda06f62cac28bf577f158d7420536d5a968e4e
Juliano <67047882+JulianoJk@users.noreply.github.com>
2022-11-29 00:48:16 +0200

    Added docker to project

--- back-end/src/controllers/users.ts
+++ back-end/src/controllers/users.ts
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-import { Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
-import jwt_decode from 'jwt-decode';
-const jwt = require('jsonwebtoken');
-const User = require('../models/user.model');
-const auth = require('../middleware/auth');
-const checkIfTokenExpired = require('../utils/utils');
-const path = require('path');
-const fs = require('fs');
+const router = require("express").Router();
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt_decode from "jwt-decode";
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+const auth = require("../middleware/auth");
+const checkIfTokenExpired = require("../utils/utils");
+const path = require("path");
+const fs = require("fs");
 
 export interface IUser {
   token?: string;
@@ -23,7 +23,7 @@ export interface IUser {
 
 let currentUser: IUser;
 
-router.get('/profile/:token', async (req: Request, res: Response) => {
+router.get("/profile/:token", async (req: Request, res: Response) => {
   try {
     const parameters = req.params;
 
@@ -32,34 +32,34 @@ router.get('/profile/:token', async (req: Request, res: Response) => {
     currentUser = await User.findById(decoded.id);
 
     if (checkIfTokenExpired(decoded.exp)) {
-      res.json('Expired session, please login again!');
+      res.json("Expired session, please login again!");
     } else {
       res.json(currentUser.dateJoined.toDateString());
     }
   } catch (error) {
-    res.status(404).json('No user found...');
+    res.status(404).json("No user found...");
   }
 });
 
-router.post('/login', async (req: Request, res: Response) => {
+router.post("/login", async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ message: 'Mandatory fields are missing.' });
+      return res.status(400).json({ message: "Mandatory fields are missing." });
     }
     const user = await User.findOne({ email: email });
     // if there is no user, inform client
     if (!user) {
-      return res.status(400).json({ message: 'No account found!' });
+      return res.status(400).json({ message: "No account found!" });
     }
     const passwordsMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordsMatch) {
-      return res.status(400).json({ message: 'Invalid username or password.' });
+      return res.status(400).json({ message: "Invalid username or password." });
     }
     const checkIfTeacher = process.env.TEACHER_EMAIL.includes(
-      'niovits22@gmail.com'
+      "niovits22@gmail.com"
     );
     //Assign the token to the user
     jwt.sign(
@@ -83,29 +83,29 @@ router.post('/login', async (req: Request, res: Response) => {
   }
 });
 
-router.post('/register', async (req: Request, res: Response) => {
+router.post("/register", async (req: Request, res: Response) => {
   try {
     let { email, password, passwordRepeat, username } = req.body;
     // Validations
     if (password !== passwordRepeat) {
-      return res.status(400).json({ message: 'Passwords do not match.' });
+      return res.status(400).json({ message: "Passwords do not match." });
     }
     if (!email || !password || !passwordRepeat) {
-      return res.status(400).json({ message: 'Mandatory fields are missing.' });
+      return res.status(400).json({ message: "Mandatory fields are missing." });
     }
     if (password.length < 5) {
       return res
         .status(400)
-        .json({ message: 'Password needs at least 5 characters.' });
+        .json({ message: "Password needs at least 5 characters." });
     }
     if (password !== passwordRepeat) {
-      return res.status(400).json({ message: 'Passwords do not match.' });
+      return res.status(400).json({ message: "Passwords do not match." });
     }
     // Check if email already exists
     const emailExists = await User.findOne({ email: email });
 
     if (emailExists) {
-      return res.status(400).json({ message: 'Email already in use.' });
+      return res.status(400).json({ message: "Email already in use." });
     }
 
     if (!username) {
@@ -147,9 +147,8 @@ router.post('/register', async (req: Request, res: Response) => {
     res.status(500).json({ error: err.message });
   }
 });
-module.exports = router;
 
-router.delete('/deleteAccount/:token', async (req: Request, res: Response) => {
+router.delete("/deleteAccount/:token", async (req: Request, res: Response) => {
   try {
     const parameters = req.params;
 
@@ -160,19 +159,19 @@ router.delete('/deleteAccount/:token', async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ message: 'Mandatory fields are missing.' });
+      return res.status(400).json({ message: "Mandatory fields are missing." });
     }
     const user = await User.findOne({ email: email });
     if (!user) {
-      return res.status(400).json({ message: 'Account not found.' });
+      return res.status(400).json({ message: "Account not found." });
     }
     const passwordsMatch = await bcrypt.compare(password, user.password);
     if (!passwordsMatch) {
-      return res.status(400).json({ message: 'Invalid username or password.' });
+      return res.status(400).json({ message: "Invalid username or password." });
     }
 
     if (checkIfTokenExpired(decoded.exp)) {
-      res.json('Expired session, please login again!');
+      res.json("Expired session, please login again!");
     } else {
       User.deleteOne({ _id: currentUser.id }, function (err: any, docs: any) {
         if (err) {
@@ -187,31 +186,31 @@ router.delete('/deleteAccount/:token', async (req: Request, res: Response) => {
   }
 });
 // Save image from FE
-router.post('/profile-image/:id', async (req: any, res: any) => {
+router.post("/profile-image/:id", async (req: any, res: any) => {
   const { id } = req.params;
   try {
     console.log(id);
 
-    if (id === 'undefined' || id === '') {
+    if (id === "undefined" || id === "") {
       res.send({
-        status: 'failed',
-        message: 'No user found',
+        status: "failed",
+        message: "No user found",
       });
     } else if (!req.files) {
       res.send({
-        status: 'failed',
-        message: 'No file uploaded',
+        status: "failed",
+        message: "No file uploaded",
       });
     } else {
       let file = req.files.file;
 
       console.log(req.params);
 
-      file.mv('./uploads/' + `${id}.png`);
+      file.mv("./uploads/" + `${id}.png`);
 
       res.send({
-        status: 'success',
-        message: 'File is uploaded',
+        status: "success",
+        message: "File is uploaded",
         data: {
           name: `${id}.png`,
           mimetype: file.mimetype,
@@ -224,7 +223,7 @@ router.post('/profile-image/:id', async (req: any, res: any) => {
   }
 });
 // Send image
-router.get('/profileImage/:id', async (req: any, res: any) => {
+router.get("/profileImage/:id", async (req: any, res: any) => {
   const { id } = req.params;
 
   const uploaded = path.resolve(__dirname, `../../uploads`);
@@ -250,3 +249,4 @@ router.get('/profileImage/:id', async (req: any, res: any) => {
     console.log(error);
   }
 });
+module.exports = router;
